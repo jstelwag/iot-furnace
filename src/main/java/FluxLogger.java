6ff2edc1fc0cd6215cb9a2261f80160e03a59f8f
Jaap Stelwagen <jaapstelwagen@gmail.com>
2016-10-13 21:15:30 +0200

    Fixed FluxLogger

--- src/main/java/FluxLogger.java
+++ src/main/java/FluxLogger.java
@@ -1,5 +1,3 @@
-import net.e175.klaus.solarpositioning.AzimuthZenithAngle;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import redis.clients.jedis.Jedis;
 
@@ -69,44 +67,12 @@ public class FluxLogger implements Closeable {
     }
 
     private void logState() {
-        if (jedis.exists("solarState")) {
-            SolarState state = SolarState.valueOf(jedis.get("solarState"));
-            String line = "solarstate,circuit=boiler500 value=";
-            line += state == SolarState.boiler500 ? "1" : "0";
+        if (jedis.exists("boiler120.state")) {
+            String line = "boiler120.state value=" + jedis.get("boiler120.state");
             send(line);
-
-            line = "solarstate,circuit=boiler200 value=";
-            line += state == SolarState.boiler200 ? "1" : "0";
-            send(line);
-
-            line = "solarstate,circuit=recycle value=";
-            line += state == SolarState.recycle ? "1" : "0";
-            send(line);
-
-            line = "solarstate,circuit=overheat value=";
-            line += state == SolarState.overheat ? "1" : "0";
-            send(line);
-
-            line = "solarstate,circuit=error value=";
-            line += state == SolarState.error ? "1" : "0";
-            send(line);
-
-            if (jedis.exists("boiler200.state")) {
-                line = "boiler200.state value=" + jedis.get("boiler200.state");
-                send(line);
-            }
         } else {
-            LogstashLogger.INSTANCE.message("ERROR: there is no SolarState in Redis");
+            LogstashLogger.INSTANCE.message("ERROR: there is no state in Redis");
         }
-    }
-
-    private void sunLogger() {
-        Sun sun = new Sun();
-        AzimuthZenithAngle position = sun.position();
-        String line = "sun azimuth=" + position.getAzimuth()
-                    + ",zenithAngle=" + position.getZenithAngle()
-                    + ",power=" + (sun.shining() ? "1" : "0");
-        send(line);
     }
 
     public FluxLogger send(String line) {
