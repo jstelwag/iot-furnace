e9e8a0205c09113c97545ac2beb25e220a0b2a2d
jstelwag <o5oJ7D30XpF2Z01hTp3v>
2019-11-30 22:01:11 +0100

    Removed system.out

--- src/main/java/handlers/RedisHandler.java
+++ src/main/java/handlers/RedisHandler.java
@@ -1,11 +1,11 @@
 package handlers;
 
-import org.apache.commons.io.IOUtils;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import redis.clients.jedis.Jedis;
+import util.LogstashLogger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -21,20 +21,20 @@ import java.util.List;
 public class RedisHandler extends AbstractHandler {
     @Override
     public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
-        System.out.println("Redis request");
-
-        Jedis jedis = new Jedis("localhost");
-        List<String> all = new ArrayList<>(jedis.keys("*"));
-        Collections.sort(all);
+        LogstashLogger.INSTANCE.info("Redis request: " + s);
 
         JSONArray redisResponse = new JSONArray();
-        for (String key : all) {
-            redisResponse.put(new JSONObject().put(key, new JSONObject()
-                    .put("value", jedis.get(key))
-                    .put("ttl", jedis.ttl(key))));
+        try (Jedis jedis = new Jedis("localhost")) {
+            List<String> all = new ArrayList<>(jedis.keys("*"));
+            Collections.sort(all);
+
+            for (String key : all) {
+                redisResponse.put(new JSONObject().put(key, new JSONObject()
+                        .put("value", jedis.get(key))
+                        .put("ttl", jedis.ttl(key))));
+            }
         }
 
-        IOUtils.closeQuietly(jedis);
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println(new JSONObject().put("dump", redisResponse).toString(2));
