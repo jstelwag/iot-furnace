3dde8c28ab178b371576bcd578a973769b646cb1
Jaap Stelwagen <jaapstelwagen@gmail.com>
2020-08-27 11:56:37 +0200

    Moved to other serial library

--- src/main/java/furnace/ListPorts.java
+++ src/main/java/furnace/ListPorts.java
@@ -1,17 +1,12 @@
 package furnace;
 
-import gnu.io.CommPortIdentifier;
-
-import java.util.Enumeration;
+import com.fazecast.jSerialComm.SerialPort;
 
 public class ListPorts {
 
     public static void print() {
-        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
-
-        while (portEnum.hasMoreElements()) {
-            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-            System.out.println("Found port: " + currPortId.getName() + ", is owned: " + currPortId.isCurrentlyOwned());
+        for (SerialPort port : SerialPort.getCommPorts()) {
+            System.out.println("Port: " + port.getSystemPortName() + " (" + port.getDescriptivePortName() + ") - is open: " + port.isOpen());
         }
     }
 }
