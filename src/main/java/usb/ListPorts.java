1e392b5a05de7e7ab15127f9ff3573f4edd4b531
jstelwag <jaapstelwagen@gmail.com>
2020-08-28 20:56:43 +0200

    Added find device functionality (usb)

--- src/main/java/usb/ListPorts.java
+++ src/main/java/usb/ListPorts.java
@@ -11,52 +11,56 @@ public class ListPorts {
 
     public static void print() {
         for (SerialPort port : SerialPort.getCommPorts()) {
-            System.out.println("Port: " + port.getSystemPortName() + " (" + port.getDescriptivePortName() + ") - echo: " + echo(port));
+            System.out.println("Port: " + port.getSystemPortName() + " (" + port.getDescriptivePortName() + ") - device: " + sniff(port));
         }
     }
 
-    public static String echo(SerialPort port) {
-        String retVal = "";
-        int sleep = 0;
+    /**
+    * Finds and returns a (closed) port for given Device.
+    **/
+    public static SerialPort findDevice(Device device) {
+        for (SerialPort port : SerialPort.getCommPorts()) {
+            if (sniff(port) == device) {
+                return port;
+            }
+        }
+        return null;
+    }
+
+    /**
+    * Listens to given port and returns the Device that is connected to it, or null.
+    **/
+    public static Device sniff(SerialPort port) {
         port.openPort();
-        port.writeBytes(new byte[]{'?'}, 1);
-        boolean waiting = true;
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING  | SerialPort.TIMEOUT_WRITE_BLOCKING, 500, 500);
+        port.writeBytes("?".getBytes(), "?".getBytes().length);
+
+        long start = System.currentTimeMillis();
+        int TIMEOUT = 2000;
         try {
-            while (waiting) {
+            while (System.currentTimeMillis() - start < TIMEOUT) {
                 while (port.bytesAvailable() == 0) {
                     Thread.sleep(20);
-                    sleep += 20;
-                    if (sleep > 2000) {
-                        waiting = false;
-                        System.out.println("Waiting too long");
+                    if (System.currentTimeMillis() - start > TIMEOUT) {
                         break;
                     }
                 }
                 if (port.bytesAvailable() > 0) {
                     byte[] readBuffer = new byte[port.bytesAvailable()];
                     port.readBytes(readBuffer, readBuffer.length);
-                    retVal += new String(readBuffer);
-                    Thread.sleep(10);
-                    sleep += 10;
-                    if (port.bytesAvailable() <= 0) {
-                        waiting = false;
+                    for (Device device : Device.values()) {
+                        if (new String(readBuffer).contains(device.name())) {
+                            port.closePort();
+                            return device;
+                        }
                     }
+                    Thread.sleep(50);
                 }
             }
-            System.out.println(retVal);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        port.closePort();
-        return retVal;
-    }
-
-    public static SerialPort findDevice(Device device) {
-        for (SerialPort port : SerialPort.getCommPorts()) {
-            String echo = echo(port);
-            if (echo != null && echo.contains(device.name())) {
-                return port;
-            }
+        } finally {
+            port.closePort();
         }
         return null;
     }
